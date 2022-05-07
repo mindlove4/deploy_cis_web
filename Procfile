25ecf8e12345b6716765ce6ecdf77c983e4e56c4
mindlove4 <60215505+mindlove4@users.noreply.github.com>
2022-05-07 22:22:11 +0700

    1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn web01.wsgi:application --log-file -
--- web01/settings.py
+++ web01/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-9yh)@wh&udltuv0@^rz41p8e52aq1oyjl7@fh5^gw=zwm6v8a_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = TRUE
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
